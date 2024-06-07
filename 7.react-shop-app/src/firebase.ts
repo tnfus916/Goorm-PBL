c79c2249c54ab108acf9355b3035cd72209f2a43
Suryun Park <tnfus1999@gmail.com>
2024-06-07 18:50:26 +0900

    Feat: Shopping app with React

---
+++ 7.react-shop-app/src/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBqRmjL1bteYmze5mjikm-ItTlm-zYS27E',
+  authDomain: 'shopping-app-fb442.firebaseapp.com',
+  projectId: 'shopping-app-fb442',
+  storageBucket: 'shopping-app-fb442.appspot.com',
+  messagingSenderId: '6987206782',
+  appId: '1:6987206782:web:596c076a8415d9a41e082f',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
